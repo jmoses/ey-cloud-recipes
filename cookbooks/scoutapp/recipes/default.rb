5f1cd62b8372c32a3914094332dd766746df4e8d
Jon Moses <jon@burningbush.us>
2010-10-26 22:10:00 -0400

    Scout only on app server

--- cookbooks/scoutapp/recipes/default.rb
+++ cookbooks/scoutapp/recipes/default.rb
@@ -5,13 +5,15 @@ require 'pp'
 #
 
 # be sure to replace "app_name" with the name of your application.
-run_for_app("ShotRunner") do |app_name, data|
-  cron "scout" do
-    minute   '*'
-    hour     '*'
-    day      '*'
-    month    '*'
-    weekday  '*'
-    command  "scout d93351dc-019e-4b53-a4b9-e99653f02b66 "
-  end  
+if ['solo', 'app', 'app_master'].include?(node[:instance_role])
+  run_for_app("ShotRunner") do |app_name, data|
+    cron "scout" do
+      minute   '*'
+      hour     '*'
+      day      '*'
+      month    '*'
+      weekday  '*'
+      command  "scout d93351dc-019e-4b53-a4b9-e99653f02b66 "
+    end  
+  end
 end
