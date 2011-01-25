7e729de168dac8ac6b8f173c56a5375dde523a7d
Jon Moses <jon@burningbush.us>
2011-01-24 20:58:22 -0500

    Try cron 2

--- cookbooks/crontab_mailto/recipes/default.rb
+++ cookbooks/crontab_mailto/recipes/default.rb
@@ -3,5 +3,5 @@
 # Recipe:: default
 #
 execute "cron-MAILTO" do
-  command "crontab -u #{node[:owner_name]} -l; if [ $? -eq 0 ] ; then (echo 'MAILTO=""'; crontab -u #{node[:owner_name]} -l) | uniq | crontab -u #{node[:owner_name]} - ; fi"
+  command %Q{crontab -u #{node[:owner_name]} -l; if [ $? -eq 0 ] ; then (echo 'MAILTO=""'; crontab -u #{node[:owner_name]} -l) | uniq | crontab -u #{node[:owner_name]} - ; fi}
 end
