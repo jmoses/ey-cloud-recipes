d3b178f52759627ab287c492b27252f5b9664427
Jon Moses <jon@burningbush.us>
2011-05-22 13:13:22 -0400

    Smarter app matching

--- cookbooks/main/attributes/recipe.rb
+++ cookbooks/main/attributes/recipe.rb
@@ -1,4 +1,4 @@
 recipes('main')
 owner_name(@attribute[:users].first[:username])
 owner_pass(@attribute[:users].first[:password])
-default[:application_name] = node[:applications].keys.first
+default[:application_name] = node[:applications].keys.find {|app| app =~ /shotrunner/i}
